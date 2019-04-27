ad6859f94dd9ffe330930a11f89e26aac1f46050
Ines CV <inescastelltort@gmail.com>
2019-04-27 15:05:41 +0200

    UPDATE login button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,8 +19,8 @@ class Navbar extends Component {
           </>
         ) : (
           <>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Signup</Link>
+            <button><Link to='/login'>Login</Link></button>
+            <button><Link to='/signup'>Signup</Link></button>
           </>
         )}
       </div>
