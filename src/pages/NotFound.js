0272c3368f70183478c2b4c2e950f9d6bcaeca9e
JdeJ <jorgedjuana@gmail.com>
2019-04-29 00:44:30 +0200

    ADD NotFound page with auth HOC

---
+++ src/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { withAuth } from "../lib/AuthProvider";
+
+function NotFound(props) {
+  console.log(props);
+  return (
+    <div>
+      <h2> Error</h2>
+      <h3><span>Congratulations {props.user.username.toUpperCase()}</span>, you broke the <del>fucking</del> site. I bet you are waiting for us to tell you what happened, well basically you did something no one liked, so <del>fuck you</del>.</h3>
+    </div>
+  )
+}
+
+export default withAuth(NotFound);
