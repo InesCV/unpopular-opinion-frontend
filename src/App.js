2a5ede1c2d20aa8ad68f891afb3e0708211f4f78
JdeJ <jorgedjuana@gmail.com>
2019-04-29 00:48:31 +0200

    UPDATE App.js to extract Router logic from it

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Opinions from "./components/Opinions";
-import CreateOpinion from "./pages/CreateOpinion";
-import Private from "./pages/Private";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-
-import PrivateRoute from "./components/PrivateRoute";
-import AnonRoute from "./components/AnonRoute";
-import AuthProvider from "./lib/AuthProvider";
 
 class App extends Component {
   render() {
     return (
-      <AuthProvider>
         <div className="container">
           <h1>Unpopular Opinion</h1>
           <Navbar />
-          <Switch>
+          {/* <Switch>
             <AnonRoute path="/signup" component={Signup} />
             <AnonRoute path="/login" component={Login} />
             <PrivateRoute path="/private" component={Private} /> 
             <Route exact path="/opinions" component={Opinions} />
             <Route exact path="/opinions/create" component={CreateOpinion} />
-          </Switch>
+          </Switch> */}
         </div>
-      </AuthProvider>
     );
   }
 }
