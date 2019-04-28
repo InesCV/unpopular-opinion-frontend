71db6fde15eb8a2db51ba67bfc112acb7b1e3983
JdeJ <jorgedjuana@gmail.com>
2019-04-29 00:49:37 +0200

    ADD Router.js to control all the routes and the context provider

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import NotFound from '../pages/NotFound'
+import AuthProvider from "../lib/AuthProvider";
+import PrivateRoute from "../components/PrivateRoute";
+import AnonRoute from "../components/AnonRoute";
+import Private from "../pages/Private";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Opinions from "../components/Opinions";
+import CreateOpinion from "../pages/CreateOpinion";
+import NotFound from '../pages/NotFound';
 
-export default () => {
-  <BrowserRouter>
-    <Switch>
-      <AnonRoute exact path="/signup" component={Signup} />
-      <AnonRoute exact path="/login" component={Login} />
-      <PrivateRoute exact path="/private" component={Private} /> 
-      <Route exact path="/opinions" component={Opinions} />
-      <Route exact path="/opinions/create" component={CreateOpinion} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  </BrowserRouter>
-}
+export default () => (
+    <BrowserRouter>
+      <Switch>
+        <AnonRoute exact path="/signup" render={ props => <AuthProvider> <Signup {...props} /> </AuthProvider> } />
+        <AnonRoute exact path="/login" render={ props => <AuthProvider> <Login {...props} /> </AuthProvider> } />
+        <PrivateRoute exact path="/private" render={ props => <AuthProvider> <Private {...props} /> </AuthProvider> } /> 
+        <Route exact path="/opinions" render={ props => <AuthProvider> <Opinions {...props} /> </AuthProvider> } />
+        <Route exact path="/opinions/create" render={ props => <AuthProvider> <CreateOpinion {...props} /> </AuthProvider> } />
+        <Route path="*" render={ props => <AuthProvider> <NotFound {...props} /> </AuthProvider> } />
+      </Switch>
+    </BrowserRouter>
+    )
