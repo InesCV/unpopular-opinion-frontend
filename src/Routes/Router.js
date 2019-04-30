5fa3bdd0ccb6493d4da0f73a606baded03a2119e
JdeJ <jorgedjuana@gmail.com>
2019-05-01 00:23:31 +0200

    ADDS Router routes mapping

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
+import routes from './routes';
 import AuthProvider from "../lib/AuthProvider";
-import PrivateRoute from "../components/PrivateRoute";
-import AnonRoute from "../components/AnonRoute";
-import App from "../App";
-import Private from "../pages/Private";
-import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import Opinions from "../components/Opinions";
-import CreateOpinion from "../pages/CreateOpinion";
-import NotFound from '../pages/NotFound';
 
 export default () => (
   <AuthProvider>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={ App } />
+        {/* <Route exact path="/" component={ App } />
         <AnonRoute exact path="/signup" component= { Signup } />
         <AnonRoute exact path="/login" component= { Login } />
         <PrivateRoute exact path="/private" component= { Private } /> 
         <Route exact path="/opinions" component= { Opinions } />
         <Route exact path="/opinions/create" component= { CreateOpinion } />
-        <Route path="*" component= { NotFound } />
+        <Route path="*" component= { NotFound } /> */}
+
+        {routes.map(({route: Route, path, component}, key) => 
+            <Route exact path={path} component={component} key={key} />
+        )}
       </Switch>
     </BrowserRouter>
   </AuthProvider>
---
+++ src/Routes/routes.js
@@ -0,0 +1,51 @@
+import { Route } from 'react-router-dom';
+
+import PrivateRoute from "../components/PrivateRoute";
+import AnonRoute from "../components/AnonRoute";
+import App from "../App";
+import Private from "../pages/Private";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Opinions from "../components/Opinions";
+import CreateOpinion from "../pages/CreateOpinion";
+import NotFound from '../pages/NotFound';
+
+const routes = [
+  {
+    route: Route,
+    path: '/',
+    component: App,
+  }, 
+  {
+    route: AnonRoute,
+    path: '/signup',
+    component: Signup,
+  }, 
+  {
+    route: AnonRoute,
+    path: '/login',
+    component: Login,
+  }, 
+  {
+    route: PrivateRoute,
+    path: '/private',
+    component: Private,
+  },
+  {
+    route: Route,
+    path: '/opinions',
+    component: Opinions,
+  },
+  {
+    route: Route,
+    path: '/opinions/create',
+    component: CreateOpinion,
+  },
+  {
+    route: Route,
+    path: '/*',
+    component: NotFound,
+  },
+];
+
+export default routes;
