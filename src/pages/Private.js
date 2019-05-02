9f254c707b9b957fe69a112e58bbfa71fde45bdc
Ines CV <inescastelltort@gmail.com>
2019-05-02 21:09:14 +0200

    ADD Navbar at private

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+
 import { withAuth } from "../lib/AuthProvider";
+import Navbar from "../components/Navbar";
+
 class Private extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         <h1>Hola {this.props.user.username}</h1>
       </div>
     );
