2e7722f65d54f08c0744df0a97f1cd94b109affe
Ines CV <inescastelltort@gmail.com>
2019-05-02 17:06:19 +0200

    ADD response functionality to question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
+import opinionService from "../lib/opinion-service";
+
 const tryout = {
   margin: 20,
   border: "1px solid black",
   padding: 20
 };
 
-export default (question) => (
+export default (question) => {
+  function resAction(res) {
+    const response = {
+      opinionId: question._id,
+      response: res,
+    }
+    opinionService.response(response)
+  }
+
+  
+  return (
   <div style={tryout}>
     <p>{question.author.username}</p>
     <p>{question.question}</p>
-    <p>{question.response.x}</p>
-    <p>{question.response.y}</p>
+    <button onClick={(e)=> {resAction('x')}}>{question.response.x}</button>
+    <button onClick={(e)=> {resAction('y')}}>{question.response.y}</button>
   </div>
-)
+  )
+}
