346853a8e5758e912a554294e9f8b0a691c22bb3
JdeJ <jorgedjuana@gmail.com>
2019-05-02 12:46:29 +0200

    ADD spinter-types helper with spiner types options

---
+++ src/lib/spiner-types.js
@@ -0,0 +1,10 @@
+export const types = {
+  Balls: "balls",
+  Bars: "bars",
+  Bubbles: "bubbles",
+  Cubes: "cubes",
+  Cylon: "cylon",
+  Spin: "spin",
+  SpinningBubbles: "spinningBubbles",
+  Spokes: "spokes",
+};
