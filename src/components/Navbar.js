7ea9ccfcc7a7ee53ffe47cc8eb4b968a4f1ee5a2
Ines CV <inescastelltort@gmail.com>
2019-05-03 19:25:42 +0200

    ADD Navbar first visual attempt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,21 @@ class Navbar extends Component {
     const { logout, isLoggedin } = this.props;
   
     return (
-      <div>
+      <div className="nav">
         {isLoggedin ? (
           <>
-            <button><Link to='/'>Home</Link></button>
-            <button><Link to='/opinions'>Opinions</Link></button>
-            <button><Link to='/opinions/create'>Create Opinion</Link></button>
-            <button><Link to='/profile'>Profile</Link></button>
-            <button><Link to='/statistics'>Statistics</Link></button>
-            <button onClick={logout}>Logout</button>
+            <button className="btn"><Link to='/'>Home</Link></button>
+            <button className="btn"><Link to='/opinions'>Opinions</Link></button>
+            <button className="btn"><Link to='/opinions/create'>Create Opinion</Link></button>
+            <button className="btn"><Link to='/profile'>Profile</Link></button>
+            <button className="btn"><Link to='/statistics'>Statistics</Link></button>
+            <button onClick={logout} className="btn">Logout</button>
             <p></p>
           </>
         ) : (
           <>
-            <button><Link to='/login'>Login</Link></button>
-            <button><Link to='/signup'>Signup</Link></button>
+            <button className="btn"><Link to='/login'>Login</Link></button>
+            <button className="btn"><Link to='/signup'>Signup</Link></button>
           </>
         )} 
       </div>
