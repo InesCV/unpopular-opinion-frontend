db763f471c413ef8e7087a89b6e80c3b62327651
Ines CV <inescastelltort@gmail.com>
2019-05-07 13:32:18 +0200

    UPDATE home default screen

--- src/App.js
+++ src/App.js
@@ -11,22 +11,10 @@ class App extends Component {
         <>
           <div className="jumbotron bg-micro full-height">
             <Navbar {...this.props}/>
-            <div className="container">
+            <div className="mt-5 pt-5 container cnt-pos flex-column">
               <h1 className="pt-5 pb-3">Unpopular Opinion</h1>
               <h4 className="mb-3">It’s a Social Network to see how popular are your thoughts within a community</h4>
               <Link className="btn btn-white" to='/signup'>Signup</Link>
-            </div>
-          </div>
-          <div className="jumbotron mid-height">
-            <div className="container">
-              <h1 className="pt-5 pb-3">Swipe & Share</h1>
-              <h4>Don't be afraid to ask the most random questions, the community will answer them for you</h4>
-            </div>
-          </div>
-          <div className="jumbotron bg-about mid-height">
-            <div className="container">
-              <h1 className="pt-5 pb-3">About us</h1>
-              <h4>It’s a Social Network to see how popular are your thoughts within a community</h4>
             </div>
           </div>
         </>
