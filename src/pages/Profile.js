cf4835e8e8caf264de2a812d34dd5359124cbb91
Ines CV <inescastelltort@gmail.com>
2019-05-09 20:29:08 +0200

    UPDATE general

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
 
+import { Link } from 'react-router-dom';
 import { withAuth } from "../lib/AuthProvider";
-import opinionService from "../lib/opinion-service";
 import {types} from "../lib/spiner-types";
 import Navbar from "../components/Navbar";
 import Spinner from "../components/Spinner";
 import UserUOPs from "../components/UserUOPs";
+import userService from "../lib/user-service";
+
 
 class Profile extends Component {
   state = {
     isLoading: false,
-    opinions: []
+    user: {}
   }
 
   componentDidMount() {
-    opinionService.notResponded()
-      .then((ops) => {
+    userService.user()
+      .then(({user}) => {
         this.setState({
           isLoading: false,
-          opinions: [...ops],
+          user,
         }) 
       })
       .catch((error)=> {
-        console.log("Couldn't get the opinions");
+        console.log("Couldn't get the user information from API.");
         console.log(error);
       });
   }
 
+
   render() {
-    const { user, logout } = this.props;
-    const { isLoading, opinions } = this.state;
+    const { logout } = this.props;
+    const { isLoading, user } = this.state;
 
     return (
       <>
@@ -41,14 +44,21 @@ class Profile extends Component {
         (<div className="container">
           <h2 className="pt-3">Hey {user.username}</h2>
           { user.description ? (<p>Your description {user.description}</p>) : (<p>You should upload a description</p>)}
-          { opinions ? 
+          { user.opinions ? 
           (<>
             { 
-              opinions.map((opinion, index) => 
+              user.opinions.map((opinion, index) => 
                 <UserUOPs key={index} index={index} op={opinion} respond={this.onRespond} />
               )
             }
-          </>) : (<p>You should upload a description</p>)}
+            <Link className="btn btn-primary" to='/opinions/create'>Create more opinions</Link>
+
+          </>) : (
+            <>
+              <p className="mb-2">You have not created a single opinion</p>
+              <Link className="btn btn-primary" to='/opinions/create'>Create Opinion</Link>
+            </>
+          )}
           <div className="d-flex justify-content-center pt-2 pb-3"><button className="btn btn-primary" onClick={logout}>Logout</button></div>
         </div>)
         }
