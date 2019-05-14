9760d6b6baad5be4ab1a46a9f4135ffe46d338d7
Ines CV <inescastelltort@gmail.com>
2019-05-14 14:27:07 +0200

    UPDATE card back only author

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -4,7 +4,7 @@ import { animated } from 'react-spring'
 const CardBack = ({cards, i, opacity, transform}) => {
   return (
     <animated.div className="card back" style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`) } }>
-      <div className="container pregunta center"><p>{cards[i].author.username}</p></div>
+      <div className="container author"><p>{cards[i].author.username}</p></div>
     </animated.div>
   )
 }
