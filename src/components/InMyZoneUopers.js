d26fd8f848dfc07f9b6fb4e54eb6abf399ae91b4
JdeJ <jorgedjuana@gmail.com>
2019-05-20 20:23:18 +0200

    ADD inmyzoneuopers component

--- src/components/InMyZoneUopers.js
+++ src/components/InMyZoneUopers.js
@@ -2,36 +2,92 @@ import React, { Component } from "react";
 
 import { withAuth } from "../lib/AuthProvider";
 
-import MatchRate from "../components/MatchRate";
+import { toast } from 'react-toastify';
+import { CircularProgressbar, CircularProgressbarWithChildren} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import '../sass/stylesheets/styles.scss';
+
+import {statTypes, errorTypes} from "../constants/constants";
+
+import statsService from '../lib/statistics-service';
 
 class InMyZoneUopers extends Component {
   state = {
-    notEnoughData: true,
+    isLoading: true,
+    notEnoughData: false,
+    match: undefined,
   }
 
-  setNotEnoughData = (value) => {
-    this.setState({
-      notEnoughData: value,
+  componentDidMount(){
+    statsService.query({
+      type: statTypes.MATCH_RATE,
+      userMatch: this.props.user._id,
+    })
+    .then(data => {
+      if (data.stats === null) {
+        this.setState({
+          match: 50,
+          notEnoughData: true,
+          isLoading: false,
+        });
+      } else {
+        this.setState({
+          match: data.avg,
+          isLoading: false,
+          notEnoughData: false,
+        });
+      }
+      console.log(data)
+    }) 
+    .catch((error)=> {
+      toast.error(`Sorry. ${errorTypes.E500S}`, {
+        position: toast.POSITION.BOTTOM_RIGHT
+      });
     });
   }
+
+  avatarStyle = {
+    backgroundImage: `url(${this.props.user.avatar})`,
+    backgroundSize: 'cover',
+    width: '70%',
+    height: '70%',
+  };
 
   render() {
     const { user } = this.props;
 
     return (
-      <div className="your-profile mb-2">
-        <div className="cnt-pos">
-          <div className="profile-user-card cnt-pos flex-column">
-            <img src={user.avatar} alt={user.username} className="card-author-img mr-2"/>
-            <MatchRate userId={user._id} username={user.username} notEnoughData={this.state.notEnoughData} setNotEnoughData={this.state.setNotEnoughData} />
-            <h2 className="profile-name pt-3">{user.username}</h2>
-            { user.description && <p className="profile-description">{user.description}</p> }
-            <div>
-              { this.state.notEnoughData && <p className="log-comment">We don't have enough answers from both users to create an <span>accurate affinity rate between you and {user.username}</span></p>}
+      <>
+        { this.state.isLoading ?
+          <div className="cnt-pos flex-column">
+            <div className="circular-prediv mt-2">
+              <CircularProgressbar value={50} text={`loading`} className="cnt-pos circular-secundary" />
             </div>
+            <p className="profile-scores-text">Your Affinity with {user.username}</p>
           </div>
-        </div>
-      </div>
+          : 
+          <>
+            { this.state.notEnoughData ?
+              <div className="cnt-pos flex-column">
+                <div className="circular-prediv mt-2">
+                  <CircularProgressbar value={50} text={`no data`} className="cnt-pos circular-red" />
+                </div>
+                <p className="profile-scores-text">Can't create your affinity with {user.username}</p>
+              </div>
+              :
+              <div className="cnt-pos flex-column">
+                <div className="circular-prediv mt-2 mb-2" >
+                  <CircularProgressbarWithChildren value={this.state.match} text={`${this.state.match}%`} className="cnt-pos circular-secundary"> 
+                    <div className="profile-imz-img" style={{ backgroundImage: `url(${user.avatar})`}}/>  
+                    
+                  </CircularProgressbarWithChildren>
+                </div>
+                <p className="profile-scores-text">Your affinity with {user.username}</p>
+              </div>      
+            }
+          </>
+        }
+      </>
     );
   }
 }
