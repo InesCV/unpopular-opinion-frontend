e515fe3c9f132e5485575b2cae219ec322ec2071
Ines CV <inescastelltort@gmail.com>
2019-05-21 01:10:45 +0200

    UPDATE Card so it does not seem a button

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -7,13 +7,15 @@ const CardFront = ({cards, i, opacity, transform}) => {
       <div className="card-container">
         <div className="pregunta"><p>{cards[i].question}</p></div>
         <div className="response">
-          <p className="response-arrow">&#171;</p>
-          <p className="response-text response-text-left">{cards[i].response.x}</p>
+          {/* <p className="response-arrow">&#171;</p> */}
+          <p className="response-arrow response-text-left secondary-color">&#171;</p>
+          <p className="response-text response-text-left secondary-color mr-2">{cards[i].response.x}</p>
+          <p className="response-text response-text-right tertiary-color ml-2">{cards[i].response.y} </p>
+          <p className="response-arrow response-text-right tertiary-color">&#187; </p>
         </div>
-        <div className="response">
-          <p className="response-text response-text-right">{cards[i].response.y}</p>
+        {/* <div className="response">
           <p className="response-arrow">&#187;</p>
-        </div>
+        </div> */}
       </div>
     </animated.div>
   )
