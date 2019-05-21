9ee5906a45b640f4f0d81143610d5ea5110aed90
JdeJ <jorgedjuana@gmail.com>
2019-05-21 18:24:41 +0200

    ADD circular stat to opinionRate

--- src/components/OpinionRate.js
+++ src/components/OpinionRate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CircularProgressbar} from 'react-circular-progressbar';
 
 
 const OpinionRate = ({skipRate, stat}) => {
@@ -6,7 +7,19 @@ const OpinionRate = ({skipRate, stat}) => {
     skipRate();
   }
   
-  return <div className="cnt-pos-total" onClick={ () => {backToOpinions()}}>You responded as {stat.stats.avg}% of our platform</div>
+  // return <div className="cnt-pos-total" onClick={ () => {backToOpinions()}}>You responded as {stat.stats.avg}% of our platform</div>
+  return (
+    <div className="cnt-pos-total" onClick={backToOpinions}>
+        <div className="cnt-pos flex-column">
+          <div className="circular-prediv mt-2 cnt-pos profile-opinion-graph-big d-flex flex-column">
+            <CircularProgressbar value={stat.stats.avg} text={`${stat.stats.avg}%`} className="cnt-pos circular-secondary" />
+          </div>
+          <div className="container mt-4">
+            <p className="mt-4">Of the users thought the same way you did</p>
+          </div>
+        </div>
+    </div>
+  )
 }
 
 export default OpinionRate;
