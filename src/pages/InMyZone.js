deacb467641ae3d8a124adc81fab9111578edd3b
JdeJ <jorgedjuana@gmail.com>
2019-05-22 10:10:14 +0200

    UPDATE inMyZone alone in zone, custom view

--- src/pages/InMyZone.js
+++ src/pages/InMyZone.js
@@ -37,7 +37,7 @@ class InMyZone extends Component {
                 <InMyZoneUser nearUopers={this.props.appStore.nearUopers.toJS()}/>
               </div>
               <div className="your-opinions">
-                <h2 className="profile-title pt-3 tertiary-color mb-2">Uopers around you</h2>
+                {(this.props.appStore.nearUopers.length > 1) && <h2 className="profile-title pt-3 tertiary-color mb-2">Uopers on 500 meters around you: </h2>}
                 <div className="container cnt-pos flex-wrap">
                   {this.props.appStore.nearUopers.toJS().map((uoper, index)=> {
                     if (uoper.username !== this.props.user.username){
