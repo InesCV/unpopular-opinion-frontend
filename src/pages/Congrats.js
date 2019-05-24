15875e8283a417e7d40458f488bcdea988c072b6
JdeJ <jorgedjuana@gmail.com>
2019-05-24 18:18:59 +0200

    DONE done done done

--- src/pages/Congrats.js
+++ src/pages/Congrats.js
@@ -8,8 +8,8 @@ class Congrats extends Component {
         <div className="jumbotron bg-about full-height d-flex justify-content-center center">
           <div className="container index-1">
             <h1 className="pt-5 pb-3">Thank you so much to my partner.</h1>
-            <h3>It was a really pleasure to work with you.</h3>
-            <p>That's all folks, be happy and vote us!</p>
+            <h3>It was a real pleasure working with you.</h3>
+            <p>That's all folks, be happy and vote for us!</p>
             <p><span className="tertiary-color bold">UOP #UnpopularOpinion</span></p>
           </div>
           <div className="overlay">
